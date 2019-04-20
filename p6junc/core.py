5e07bcee3a09e8aeb639ae4c19fc60f24f6c8a3c
Andrew Palmer <atp@sdf.org>
2019-04-20 00:15:44 -0500

    Support "==" operator

--- p6junc/core.py
+++ p6junc/core.py
@@ -5,8 +5,17 @@ class _Junction(object):
         self._bool = to_bool
         self._items = items
 
+    def __iter__(self):
+        return iter(self._items)
+
+    def __repr__(self):
+        return self._bool.__name__ + str(self._items)
+
     def __bool__(self):
         return self._bool(self._items)
+
+    def __eq__(self, other):
+        return self._bool(other._bool(a.__eq__(b) for b in other) for a in self)
 
 
 def any(*items):
