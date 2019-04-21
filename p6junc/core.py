5f71683a1f4f6a2f3a0d6e3db3eeb1ff4172c7da
Andrew Palmer <atp@sdf.org>
2019-04-20 19:21:45 -0500

    Introduce precedence

--- p6junc/core.py
+++ p6junc/core.py
@@ -1,3 +1,19 @@
+from collections import OrderedDict
+
+
+BOOL_CONVERTERS_BY_PRECEDENCE = OrderedDict([
+    ('any', __builtins__['any']),
+    ('all', __builtins__['all']),
+])
+
+
+def find_precedence(a, b):
+    for func in BOOL_CONVERTERS_BY_PRECEDENCE.values():
+        if a._bool is func:
+            return (a, b)
+        if b._bool is func:
+            return (b, a)
+    assert False, 'Should not arrive here'
 
 
 class _Junction(object):
@@ -12,15 +28,16 @@ class _Junction(object):
         return self._bool.__name__ + str(self._items)
 
     def __bool__(self):
-        return self._bool(self._items)
+        return self._bool(self)
 
     def __eq__(self, other):
-        return self._bool(other._bool(a.__eq__(b) for b in other) for a in self)
+        inner, outer = find_precedence(self, other)
+        return outer._bool(inner._bool(a.__eq__(b) for a in inner) for b in outer)
 
 
 def any(*items):
-    return _Junction(items=items, to_bool=__builtins__['any'])
+    return _Junction(items=items, to_bool=BOOL_CONVERTERS_BY_PRECEDENCE['any'])
 
 
 def all(*items):
-    return _Junction(items=items, to_bool=__builtins__['all'])
+    return _Junction(items=items, to_bool=BOOL_CONVERTERS_BY_PRECEDENCE['all'])
--- test/test_p6junc.py
+++ test/test_p6junc.py
@@ -51,3 +51,11 @@ def test_any123_eq_any345():
 
 def test_all1_eq_all12():
     assert junc.all(1) != junc.all(1,2)
+
+
+def test_any12_eq_all12():
+    assert junc.any(1,2) == junc.all(1,2)
+
+
+def test_all12_eq_any12():
+    assert junc.all(1,2) == junc.any(1,2)
