de1608acc0506aa562f0a7b78c3df8c642f80f58
Andrew Palmer <atp@sdf.org>
2020-09-26 15:59:40 -0500

    Set bool converters statically

--- p6junc/core.py
+++ p6junc/core.py
@@ -1,16 +1,6 @@
 from collections import OrderedDict
 
 
-def _one(iterable):
-    count = 0
-    for item in iterable:
-        if item:
-            count += 1
-        if count > 1:
-            return False
-    return count == 1
-
-
 def find_precedence(a, b):
     for cls in (_Any, _One, _All):
         if isinstance(a, cls):
@@ -21,47 +11,54 @@ def find_precedence(a, b):
 
 
 class _Junction(object):
-    def __init__(self, *, to_bool, items):
-        self._bool = to_bool
+    def __init__(self, *items):
         self._items = items
 
     def __iter__(self):
         return iter(self._items)
 
     def __repr__(self):
-        return self._bool.__name__ + str(self._items)
+        return self.bool.__name__ + str(self._items)
 
     def __bool__(self):
-        return self._bool(self)
+        return self.bool(self)
 
     def __eq__(self, other):
         inner, outer = find_precedence(self, other)
-        return outer._bool(inner._bool(a.__eq__(b) for a in inner) for b in outer)
+        return outer.bool(inner.bool(a.__eq__(b) for a in inner) for b in outer)
 
     def __gt__(self, other):
         inner, outer = find_precedence(self, other)
-        return outer._bool(inner._bool(a.__gt__(b) for a in inner) for b in outer)
+        return outer.bool(inner.bool(a.__gt__(b) for a in inner) for b in outer)
 
 
 class _Any(_Junction):
-    pass
+    bool = __builtins__['any']
 
 
 class _All(_Junction):
-    pass
+    bool = __builtins__['all']
 
 
 class _One(_Junction):
-    pass
+    @staticmethod
+    def bool(iterable):
+        count = 0
+        for item in iterable:
+            if item:
+                count += 1
+            if count > 1:
+                return False
+        return count == 1
 
 
 def any(*items):
-    return _Any(items=items, to_bool=__builtins__['any'])
+    return _Any(*items)
 
 
 def all(*items):
-    return _All(items=items, to_bool=__builtins__['all'])
+    return _All(*items)
 
 
 def one(*items):
-    return _One(items=items, to_bool=_one)
+    return _One(*items)
