31c45901d30acb40785d08421502b4ca572a09ce
Andrew Palmer <atp@sdf.org>
2020-09-26 21:43:06 -0500

    Combine tests

--- test/test_all.py
+++ test/test_all.py
@@ -55,3 +55,63 @@ def test_one_FalseTrueFalse():
 
 def test_one_FalseFalseFalse():
     assert bool(junc.one(False, False, False)) == False
+
+
+def test_any123_eq_any345():
+    assert junc.any(1,2,3) == junc.any(3,4,5)
+
+
+def test_all1_eq_all12():
+    assert (junc.all(1) == junc.all(1,2)) == False
+
+
+def test_any12_eq_all12():
+    assert junc.any(1,2) == junc.all(1,2)
+
+
+def test_all12_eq_any12():
+    assert junc.all(1,2) == junc.any(1,2)
+
+
+def test_one12_eq_one12():
+    assert (junc.one(1,2) == junc.one(1,2)) == False
+
+
+def test_one12_eq_any12():
+    assert (junc.one(1,2) == junc.any(1,2)) == False
+
+
+def test_one12_eq_all12():
+    assert junc.one(1,2) == junc.all(1,2)
+
+
+def test_all_gt_all():
+    assert junc.all(1,2,3) > junc.all(-1,-2,-3)
+
+
+def test_all_gt_any():
+    assert junc.all(1,2,3) > junc.any(-1,4,5)
+
+
+def test_any_gt_all():
+    assert junc.any(-1,-2,4) > junc.all(1,2,3)
+
+
+def test_all_lt_any():
+    assert junc.all(1,2,3) < junc.any(-1,-2,4)
+
+
+def test_any123_ne_any345():
+    assert (junc.any(1,2,3) != junc.any(3,4,5)) == False
+
+
+def test_all1_ne_all12():
+    assert (junc.all(1) != junc.all(1,2)) == True
+
+
+def test_any12_ne_all12():
+    assert (junc.any(1,2) != junc.all(1,2)) == False
+
+
+def test_all12_ne_any12():
+    assert (junc.all(1,2) != junc.any(1,2)) == False
