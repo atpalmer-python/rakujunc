3290b1dd77941a804259ea3cfb63680680c387f2
Andrew Palmer <atp@sdf.org>
2020-09-28 00:32:46 -0500

    Reorganize tests

--- test/test_all.py
+++ test/test_all.py
@@ -1,56 +1,69 @@
 import p6junc as junc
 
 
+# composition
+
+
+def test_comp_any_allTrue_allFalse():
+    assert bool(junc.any(junc.all(True), junc.all(False))) == True
+
+
+def test_comp_any_allTrue_anyTrueFalse():
+    assert bool(junc.any(junc.all(True), junc.any(True, False))) == True
+
+
+def test_comp_any_allTrueFalse_anyFalse():
+    assert bool(junc.any(junc.all(True, False), junc.any(False))) == False
+
+
+def test_comp_all_allTrue_allTrue():
+    assert bool(junc.all(junc.all(True), junc.all(True))) == True
+
+
+def test_comp_all_allTrue_allFalse():
+    assert bool(junc.all(junc.all(True), junc.all(False))) == False
+
+
 # any
 
 
 def test_any_FalseTrueFalse():
-    assert bool(junc.any(False, True, False)) == True
+    assert junc.any(False, True, False)
 
 
 def test_any_FalseFalse():
-    assert bool(junc.any(False, False)) == False
+    assert not junc.any(False, False)
 
 
 def test_any_empty():
-    assert bool(junc.any()) == False
+    assert not junc.any()
 
 
-def test_any_allTrue_allFalse():
-    assert bool(junc.any(junc.all(True), junc.all(False))) == True
-
-
-def test_any_allTrue_anyTrueFalse():
-    assert bool(junc.any(junc.all(True), junc.any(True, False))) == True
-
-
-def test_any_allTrueFalse_anyFalse():
-    assert bool(junc.any(junc.all(True, False), junc.any(False))) == False
-
+## any v. any
 
 def test_any123_eq_any345():
     assert junc.any(1,2,3) == junc.any(3,4,5)
 
 
-def test_all1_ne_all12():
-    assert junc.all(1) != junc.all(1,2)
+def test_any123_ne_any345():
+    assert not (junc.any(1,2,3) != junc.any(3,4,5))
 
+
+## any v. all
 
 def test_any12_eq_all12():
     assert junc.any(1,2) == junc.all(1,2)
+
+
+def test_any12_ne_all12():
+    assert not (junc.any(1,2) != junc.all(1,2))
 
 
 def test_any_gt_all():
     assert junc.any(-1,-2,4) > junc.all(1,2,3)
 
 
-def test_any123_ne_any345():
-    assert (junc.any(1,2,3) != junc.any(3,4,5)) == False
-
-
-def test_any12_ne_all12():
-    assert (junc.any(1,2) != junc.all(1,2)) == False
-
+## any v. none
 
 def test_any_ne_none():
     assert junc.any(1,2) != junc.none(1,2)
@@ -64,31 +77,31 @@ def test_any_gt_none():
 
 
 def test_all_TrueTrue():
-    assert bool(junc.all(True, True)) == True
+    assert junc.all(True, True)
 
 
 def test_all_TrueTrueFalse():
-    assert bool(junc.all(True, True, False)) == False
+    assert not junc.all(True, True, False)
 
 
 def test_all_empty():
-    assert bool(junc.all()) == True
+    assert junc.all()
 
 
-def test_all_allTrue_allTrue():
-    assert bool(junc.all(junc.all(True), junc.all(True))) == True
+## all v. all
 
-
-def test_all_allTrue_allFalse():
-    assert bool(junc.all(junc.all(True), junc.all(False))) == False
-
-
-def test_all12_eq_any12():
-    assert junc.all(1,2) == junc.any(1,2)
+def test_all1_ne_all12():
+    assert junc.all(1) != junc.all(1,2)
 
 
 def test_all_gt_all():
     assert junc.all(1,2,3) > junc.all(-1,-2,-3)
+
+
+## all v. any
+
+def test_all12_eq_any12():
+    assert junc.all(1,2) == junc.any(1,2)
 
 
 def test_all_gt_any():
@@ -100,8 +113,10 @@ def test_all_lt_any():
 
 
 def test_all12_ne_any12():
-    assert (junc.all(1,2) != junc.any(1,2)) == False
+    assert not (junc.all(1,2) != junc.any(1,2))
 
+
+## all v. none
 
 def test_all_eq_none():
     assert junc.all(1,3,5,7,9) == junc.none(2,4,6,8,10)
@@ -115,7 +130,7 @@ def test_all_lt_none():
 
 
 def test_one_TrueTrueFalse():
-    assert bool(junc.one(True, True, False)) == False
+    assert not junc.one(True, True, False)
 
 
 def test_one_FalseTrueFalse():
@@ -126,17 +141,25 @@ def test_one_FalseFalseFalse():
     assert bool(junc.one(False, False, False)) == False
 
 
-def test_one12_eq_one12():
-    assert (junc.one(1,2) == junc.one(1,2)) == False
+## one v. one
+
+def test_one12_ne_one12():
+    assert junc.one(1,2) != junc.one(1,2)
 
 
-def test_one12_eq_any12():
-    assert (junc.one(1,2) == junc.any(1,2)) == False
+## one v. any
 
+def test_one12_ne_any12():
+    assert junc.one(1,2) != junc.any(1,2)
+
+
+## one v. all
 
 def test_one12_eq_all12():
     assert junc.one(1,2) == junc.all(1,2)
 
+
+## one v. none
 
 def test_one_eq_none():
     assert junc.one(1,2) != junc.none(1,2)
@@ -149,6 +172,14 @@ def test_one_gt_none():
 # none
 
 
+## none v. none
+
+def test_none_gt_none():
+    junc.none(1, 2) > junc.all(5, 6)
+
+
+## none v. any
+
 def test_none_eq_any():
     junc.none(1,2) == junc.any(5,6)
 
@@ -157,13 +188,11 @@ def test_none_gt_any():
     junc.none(1,2) > junc.any(5,6)
 
 
+## none v. all
+
 def test_none_gt_all():
     junc.none(1,2) > junc.all(5,6)
 
 
 def test_none_gt_one():
     junc.none(1,2) > junc.all(5,6)
-
-
-def test_none_gt_none():
-    junc.none(1, 2) > junc.all(5, 6)
