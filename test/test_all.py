f2fa1d057ff823234568e5c81dfc5f7ff1c31553
Andrew Palmer <atp@sdf.org>
2020-10-02 18:11:59 -0500

    Add transitivity tests

--- test/test_all.py
+++ test/test_all.py
@@ -61,6 +61,20 @@ def test_class():
     assert junc.all(x2) != x3
 
 
+# transitivity
+
+
+def test_transitivity_all_v_int():
+    assert junc.all(42) == 42
+    assert junc.all(42) != 0
+
+
+def test_transitivity_int_v_all():
+    assert 42 == junc.all(42)
+    assert 0 != junc.all(42)
+
+
+
 # composition
 
 
