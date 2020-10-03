d373234897f8767a4b95403acbfb505050882779
Andrew Palmer <atp@sdf.org>
2020-10-03 11:22:00 -0500

    Break out test_composition.py

--- test/test_all.py
+++ test/test_all.py
@@ -1,29 +1,6 @@
 import rakujunc as junc
 
 
-# composition
-
-
-def test_comp_any_allTrue_allFalse():
-    assert junc.any(junc.all(True), junc.all(False))
-
-
-def test_comp_any_allTrue_anyTrueFalse():
-    assert junc.any(junc.all(True), junc.any(True, False))
-
-
-def test_comp_any_allTrueFalse_anyFalse():
-    assert not junc.any(junc.all(True, False), junc.any(False))
-
-
-def test_comp_all_allTrue_allTrue():
-    assert junc.all(junc.all(True), junc.all(True))
-
-
-def test_comp_all_allTrue_allFalse():
-    assert not junc.all(junc.all(True), junc.all(False))
-
-
 # any
 
 
