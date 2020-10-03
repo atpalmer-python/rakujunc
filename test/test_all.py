f39695554cd5b9c167ec40ac5c89f2634032ae0c
Andrew Palmer <atp@sdf.org>
2020-10-03 11:25:07 -0500

    Break out test_any.py

--- test/test_all.py
+++ test/test_all.py
@@ -1,55 +1,6 @@
 import rakujunc as junc
 
 
-# any
-
-
-def test_any_FalseTrueFalse():
-    assert junc.any(False, True, False)
-
-
-def test_any_FalseFalse():
-    assert not junc.any(False, False)
-
-
-def test_any_empty():
-    assert not junc.any()
-
-
-## any v. any
-
-def test_any123_eq_any345():
-    assert junc.any(1,2,3) == junc.any(3,4,5)
-
-
-def test_any123_ne_any345():
-    assert not (junc.any(1,2,3) != junc.any(3,4,5))
-
-
-## any v. all
-
-def test_any12_eq_all12():
-    assert junc.any(1,2) == junc.all(1,2)
-
-
-def test_any12_ne_all12():
-    assert not (junc.any(1,2) != junc.all(1,2))
-
-
-def test_any_gt_all():
-    assert junc.any(-1,-2,4) > junc.all(1,2,3)
-
-
-## any v. none
-
-def test_any_ne_none():
-    assert junc.any(1,2) != junc.none(1,2)
-
-
-def test_any_gt_none():
-    assert junc.any(1,2,10) > junc.none(11,12,13)
-
-
 # all
 
 
