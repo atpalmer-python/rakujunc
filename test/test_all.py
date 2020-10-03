dc1509472696e869c7b3307a4b48a9d7162988b9
Andrew Palmer <atp@sdf.org>
2020-10-02 21:15:16 -0500

    Break out test_transitivity.py

--- test/test_all.py
+++ test/test_all.py
@@ -1,19 +1,6 @@
 import rakujunc as junc
 
 
-# transitivity
-
-
-def test_transitivity_all_v_int():
-    assert junc.all(42) == 42
-    assert junc.all(42) != 0
-
-
-def test_transitivity_int_v_all():
-    assert 42 == junc.all(42)
-    assert 0 != junc.all(42)
-
-
 # composition
 
 
