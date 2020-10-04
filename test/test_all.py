473f3f308b7dec8199f67de52f0c0871fd3c1d87
Andrew Palmer <atp@sdf.org>
2020-10-03 22:28:53 -0500

    Break out test_one.py

--- test/test_all.py
+++ test/test_all.py
@@ -54,49 +54,6 @@ def test_all_lt_none():
     assert junc.all(11,12,13) < junc.none(9,8,7)
 
 
-# one
-
-
-def test_one_TrueTrueFalse():
-    assert not junc.one(True, True, False)
-
-
-def test_one_FalseTrueFalse():
-    assert bool(junc.one(False, True, False)) == True
-
-
-def test_one_FalseFalseFalse():
-    assert bool(junc.one(False, False, False)) == False
-
-
-## one v. one
-
-def test_one12_ne_one12():
-    assert junc.one(1,2) != junc.one(1,2)
-
-
-## one v. any
-
-def test_one12_ne_any12():
-    assert junc.one(1,2) != junc.any(1,2)
-
-
-## one v. all
-
-def test_one12_eq_all12():
-    assert junc.one(1,2) == junc.all(1,2)
-
-
-## one v. none
-
-def test_one_eq_none():
-    assert junc.one(1,2) != junc.none(1,2)
-
-
-def test_one_gt_none():
-    assert junc.one(1,2) > junc.none(5,6,7)
-
-
 # none
 
 
---
+++ test/test_one.py
@@ -0,0 +1,42 @@
+import rakujunc as junc
+
+
+def test_one_TrueTrueFalse():
+    assert not junc.one(True, True, False)
+
+
+def test_one_FalseTrueFalse():
+    assert bool(junc.one(False, True, False)) == True
+
+
+def test_one_FalseFalseFalse():
+    assert bool(junc.one(False, False, False)) == False
+
+
+## one v. one
+
+def test_one12_ne_one12():
+    assert junc.one(1,2) != junc.one(1,2)
+
+
+## one v. any
+
+def test_one12_ne_any12():
+    assert junc.one(1,2) != junc.any(1,2)
+
+
+## one v. all
+
+def test_one12_eq_all12():
+    assert junc.one(1,2) == junc.all(1,2)
+
+
+## one v. none
+
+def test_one_eq_none():
+    assert junc.one(1,2) != junc.none(1,2)
+
+
+def test_one_gt_none():
+    assert junc.one(1,2) > junc.none(5,6,7)
+
