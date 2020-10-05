3aa940832f8c05018b897d486e2519cc3ff524ab
Andrew Palmer <atp@sdf.org>
2020-10-04 23:06:08 -0500

    Cleanup 'all' tests

--- test/test_all.py
+++ test/test_all.py
@@ -2,40 +2,6 @@ import pytest
 import rakujunc as junc
 
 
-def test_all_repr():
-    assert repr(junc.all('hello', 'world')) == "all('hello', 'world')"
-
-
-def test_all_str():
-    assert str(junc.all('hello', 'world')) == "all('hello', 'world')"
-
-
-# bool
-
-
-def test_all_empty():
-    assert junc.all()
-
-
-def test_all_TrueTrue():
-    assert junc.all(True, True)
-
-
-def test_all_TrueTrueFalse():
-    assert not junc.all(True, True, False)
-
-
-def test_all_TrueTrueNone():
-    assert not junc.all(True, True, None)
-
-
-def test_all_TrueTrue0():
-    assert not junc.all(True, True, 0)
-
-
-# mixed types
-
-
 def test_all_mixed_int_bool():
     assert junc.all(1) == True
     assert junc.all(0) == False
@@ -48,47 +14,54 @@ def test_all_mixed_str_bool():
         assert junc.all('') == False
 
 
-## all v. all
+def test_all_repr():
+    assert repr(junc.all('hello', 'world')) == "all('hello', 'world')"
 
 
-def test_all1_ne_all12():
-    assert junc.all(1) != junc.all(1,2)
+def test_all_str():
+    assert str(junc.all('hello', 'world')) == "all('hello', 'world')"
 
 
-def test_all_gt_all():
-    assert junc.all(1,2,3) > junc.all(-1,-2,-3)
+def test_all_empty():
+    assert junc.all()
 
 
-## all v. any
+def test_all_True():
+    assert junc.all(True, True)
 
-def test_all12_eq_any12():
+
+def test_all_False():
+    assert not junc.all(True, True, False)
+
+
+def test_all_eq_all():
+    assert junc.all(1,1) == junc.all(1,1)
+
+
+def test_all_ne_all():
+    assert junc.all(1,1) != junc.all(1,2)
+
+
+def test_all_eq_any():
     assert junc.all(1,2) == junc.any(1,2)
 
 
-def test_all_gt_any():
-    assert junc.all(1,2,3) > junc.any(-1,4,5)
+def test_all_ne_any():
+    assert junc.all(1,2) != junc.any(1,3)
 
 
-def test_all_lt_any():
-    assert junc.all(1,2,3) < junc.any(-1,-2,4)
-
-
-def test_all12_ne_any12():
-    assert not (junc.all(1,2) != junc.any(1,2))
-
-
-## all v. one
-
-def test_all12_eq_one12():
+def test_all_eq_one():
     assert junc.all(1,2) == junc.one(1,2)
 
 
-## all v. none
+def test_all_ne_one():
+    assert junc.all(1,2) != junc.one(1,3)
+
 
 def test_all_eq_none():
     assert junc.all(1,3,5,7,9) == junc.none(2,4,6,8,10)
 
 
-def test_all_lt_none():
-    assert junc.all(11,12,13) < junc.none(9,8,7)
+def test_all_eq_none():
+    assert junc.all(1,3,5,7,9) != junc.none(1,2,3)
 
