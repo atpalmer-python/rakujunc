44356a158ce591b687e64291c68a0ac28efa591e
Andrew Palmer <atp@sdf.org>
2020-10-04 22:28:34 -0500

    Cleanup one tests

--- test/test_one.py
+++ test/test_one.py
@@ -9,42 +9,42 @@ def test_one_str():
     assert str(junc.one('hello', 'world')) == "one('hello', 'world')"
 
 
-def test_one_TrueTrueFalse():
-    assert not junc.one(True, True, False)
+def test_one_True():
+    assert junc.one(False, True, False)
 
 
-def test_one_FalseTrueFalse():
-    assert bool(junc.one(False, True, False)) == True
+def test_one_False():
+    assert not junc.one(True, True, True)
 
 
-def test_one_FalseFalseFalse():
-    assert bool(junc.one(False, False, False)) == False
+def test_one_eq_one():
+    assert junc.one(1,2) == junc.one(1,3)
 
 
-## one v. one
-
-def test_one12_ne_one12():
+def test_one_ne_one():
     assert junc.one(1,2) != junc.one(1,2)
 
 
-## one v. any
+def test_one_eq_any():
+    assert junc.one(1,2) == junc.any(1,3)
 
-def test_one12_ne_any12():
+
+def test_one_ne_any():
     assert junc.one(1,2) != junc.any(1,2)
 
 
-## one v. all
-
-def test_one12_eq_all12():
+def test_one_eq_all():
     assert junc.one(1,2) == junc.all(1,2)
 
 
-## one v. none
+def test_one_ne_all():
+    assert junc.one(1,2) != junc.all(1,3)
+
 
 def test_one_eq_none():
+    assert junc.one(1,2) == junc.none(3)
+
+
+def test_one_ne_none():
     assert junc.one(1,2) != junc.none(1,2)
-
-
-def test_one_gt_none():
-    assert junc.one(1,2) > junc.none(5,6,7)
 
