972034848184873f860003fcde9f4d9614bf1783
Andrew Palmer <atp@sdf.org>
2020-10-04 22:49:04 -0500

    Test empty none junction

--- test/test_none.py
+++ test/test_none.py
@@ -9,6 +9,10 @@ def test_none_str():
     assert str(junc.none('hello', 'world')) == "none('hello', 'world')"
 
 
+def test_none_empty():
+    assert junc.none()
+
+
 def test_none_True():
     assert junc.none(False, False, False)
 
