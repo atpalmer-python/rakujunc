773f5dfad01ea987d29d66363c82a88ab86df3b7
Andrew Palmer <atp@sdf.org>
2020-10-04 22:48:11 -0500

    Cleanup any tests

--- test/test_any.py
+++ test/test_any.py
@@ -9,54 +9,46 @@ def test_any_str():
     assert str(junc.any('hello', 'world')) == "any('hello', 'world')"
 
 
-def test_any_FalseTrueFalse():
-    assert junc.any(False, True, False)
-
-
-def test_any_FalseFalse():
-    assert not junc.any(False, False)
-
-
 def test_any_empty():
     assert not junc.any()
 
 
-## any v. any
-
-def test_any123_eq_any345():
-    assert junc.any(1,2,3) == junc.any(3,4,5)
+def test_any_True():
+    assert junc.any(False, True, False)
 
 
-def test_any123_ne_any345():
-    assert not (junc.any(1,2,3) != junc.any(3,4,5))
+def test_any_False():
+    assert not junc.any(False, False)
 
 
-## any v. all
+def test_any_eq_any():
+    assert junc.any(1,2) == junc.any(2,4)
 
-def test_any12_eq_all12():
+
+def test_any_ne_any():
+    assert junc.any(1,2) != junc.any(3,4)
+
+
+def test_any_eq_all():
     assert junc.any(1,2) == junc.all(1,2)
 
 
-def test_any12_ne_all12():
-    assert not (junc.any(1,2) != junc.all(1,2))
+def test_any_ne_all():
+    assert junc.any(1,2) != junc.all(1,3)
 
-
-def test_any_gt_all():
-    assert junc.any(-1,-2,4) > junc.all(1,2,3)
-
-
-## any v. one
 
 def test_any_eq_one():
     assert junc.any(1,2) == junc.one(1,3)
 
 
-## any v. none
+def test_any_ne_one():
+    assert junc.any(1,2) != junc.one(3,4)
+
+
+def test_any_eq_none():
+    assert junc.any(1,2) == junc.none(3,4)
+
 
 def test_any_ne_none():
-    assert junc.any(1,2) != junc.none(1,2)
-
-
-def test_any_gt_none():
-    assert junc.any(1,2,10) > junc.none(11,12,13)
+    assert junc.any(1,2) != junc.none(1,3)
 
