99d092ec6ed8b4883b919b4091e927afbcaefcfc
Andrew Palmer <atp@sdf.org>
2020-10-04 22:15:11 -0500

    Simplify test case approach

--- test/test_none.py
+++ test/test_none.py
@@ -9,33 +9,29 @@ def test_none_str():
     assert str(junc.none('hello', 'world')) == "none('hello', 'world')"
 
 
-## none v. none
-
-def test_none_gt_none():
-    assert junc.none(1, 2) > junc.all(5, 6)
+def test_none_eq_none():
+    assert junc.none(1,2) == junc.none(1,2)
 
 
-## none v. any
+def test_none_ne_none():
+    assert junc.none(1,2) != junc.none(3,4)
+
 
 def test_none_eq_any():
-    assert junc.none(1,2) == junc.any(5,6)
+    assert junc.none(1,2) == junc.any(3,4)
 
 
-def test_none_gt_any():
-    assert junc.none(1,2) > junc.any(5,6)
+def test_none_ne_any():
+    assert junc.none(1,2) != junc.any(1,2)
 
 
-## none v. all
-
-def test_none_gt_all():
-    assert junc.none(1,2) > junc.all(5,6)
+def test_none_eq_all():
+    assert junc.none(1,2) == junc.all(3,4)
 
 
-def test_none_gt_one():
-    assert junc.none(1,2) > junc.all(5,6)
+def test_none_ne_all():
+    assert junc.none(1) != junc.all(1)
 
-
-## none v. one
 
 def test_none_eq_one():
     assert junc.none(1,2) == junc.one(3,4,5)
