673a288d47799cf6087c074ca31bbb1e6489fb73
Andrew Palmer <atp@sdf.org>
2020-10-04 21:58:17 -0500

    Fix none assertions

--- test/test_none.py
+++ test/test_none.py
@@ -12,27 +12,27 @@ def test_none_str():
 ## none v. none
 
 def test_none_gt_none():
-    junc.none(1, 2) > junc.all(5, 6)
+    assert junc.none(1, 2) > junc.all(5, 6)
 
 
 ## none v. any
 
 def test_none_eq_any():
-    junc.none(1,2) == junc.any(5,6)
+    assert junc.none(1,2) == junc.any(5,6)
 
 
 def test_none_gt_any():
-    junc.none(1,2) > junc.any(5,6)
+    assert junc.none(1,2) > junc.any(5,6)
 
 
 ## none v. all
 
 def test_none_gt_all():
-    junc.none(1,2) > junc.all(5,6)
+    assert junc.none(1,2) > junc.all(5,6)
 
 
 def test_none_gt_one():
-    junc.none(1,2) > junc.all(5,6)
+    assert junc.none(1,2) > junc.all(5,6)
 
 
 ## none v. one
